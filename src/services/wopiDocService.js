b4255aab1d315e5c77bdab3f761ef5f2523b3e2b
Cristhian Vargas Flores <cristhiansergiovargasflores@gmail.com>
2026-05-25 12:02:18 -0400

    Actulizacion de coneccion a collabora APP

--- src/services/wopiDocService.js
+++ src/services/wopiDocService.js
@@ -6,13 +6,16 @@ const API_URL = `${import.meta.env.VITE_API_URL}/wopi`;
 
 const API_URL_DOCSUELTOS = `${import.meta.env.VITE_API_URL}/docsueltos`;
 
+//const direccionCollaboraTest = `https://office.cumbre.com.bo/browser/dist/cool.html`
+const direccionCollaboraProduccion = `https://office.legaly.local/browser/dist/cool.html`
+
 const wopiURL = (docId) => {
-  const URL = `https://office.cumbre.com.bo/browser/dist/cool.html?WOPISrc=${API_URL}/files/${docId}`;
+  const URL = `${direccionCollaboraProduccion}?WOPISrc=${API_URL}/files/${docId}`;
   return URL;
 };
 
 const wopiURLDocSueltos = (docId) => {
-  const URL = `https://office.cumbre.com.bo/browser/dist/cool.html?WOPISrc=${API_URL_DOCSUELTOS}/files/${docId}`;
+  const URL = `${direccionCollaboraProduccion}?WOPISrc=${API_URL_DOCSUELTOS}/files/${docId}`;
   return URL;
 };
 
